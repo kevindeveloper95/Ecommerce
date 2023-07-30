c046834803fb373673bb4f63f714ff74c1171649
kevin <kemen95@hotmail.com>
2023-07-29 22:00:44 -0600

    I create a few screens and the server in the backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express, { response } from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+import products from './data/products.js';
+import { request } from 'http'
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+
+app.get("/", (req, res) =>{
+    res.send('API running')
+
+})
+
+app.get("/api/products", (req, res) =>{
+    res.json(products)
+
+})
+
+app.get("/api/products/:id", (req, res) =>{
+    const product = products.find((p) => p._id === req.params.id) 
+    res.json(product)
+
+})
+
+app.listen(port, () => console.log("Server working in port 5000"))
