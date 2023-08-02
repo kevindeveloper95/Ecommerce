b61014b08497fb00e2a77d4d80bd26ffbc411cce
kevin <kemen95@hotmail.com>
2023-08-01 21:18:03 -0600

    Get products from database

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,23 @@
 import express, { response } from 'express';
 import dotenv from "dotenv";
 dotenv.config();
-import products from './data/products.js';
 import { request } from 'http'
 import conectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 
 conectDB();
 
 const port = process.env.PORT || 5000
 
-const app = express()
-
+const app = express();
 
 app.get("/", (req, res) =>{
     res.send('API running')
 
 })
 
-app.get("/api/products", (req, res) =>{
-    res.json(products)
 
-})
-
-app.get("/api/products/:id", (req, res) =>{
-    const product = products.find((p) => p._id === req.params.id) 
-    res.json(product)
-
-})
+app.use('/api/products', productRoutes)
 
 app.listen(port, () => console.log("Server working in port 5000"))
