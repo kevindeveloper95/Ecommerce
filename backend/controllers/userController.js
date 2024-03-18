1a045a43db4379191cf2464fd9c5c6062e479e7b
kevin <kemen95@hotmail.com>
2024-03-17 20:57:31 -0600

    added json web token and cookies and all de validations

---
+++ backend/controllers/userController.js
@@ -0,0 +1,108 @@
+import asyncHandler from '../middlewares/asyncHandler.js';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
+
+
+
+const authUser = asyncHandler(async (req, res) => {
+const {email, password} = req.body
+
+const user = await User.findOne({email})
+
+if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id)
+
+
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+    })
+} else {
+    res.status(401)
+    throw new Error("invalid email or password")
+}
+});
+
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    const userExists = await User.findOne({email});
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    })
+
+    if (user) {
+        generateToken(res, user._id)
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        }) 
+    }else{
+        res.status(400);
+        throw new Error("Invalid user data")
+    }
+
+
+
+   });
+
+
+
+   const logoutUser = asyncHandler(async (req, res) => {
+   res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0)
+   })
+
+   res.status(200).json({message: "Logged out successfully"})
+   });
+
+   const getUserProfile = asyncHandler(async (req, res) => {
+    res.send("get user profile")
+   });
+
+   const getUsers = asyncHandler(async (req, res) => {
+    res.send("get users")
+   });
+
+   const updateUsersProfile = asyncHandler(async (req, res) => {
+    res.send("update user profile")
+   });
+
+   const getUsersById = asyncHandler(async (req, res) => {
+    res.send("get users by id")
+   });
+
+   const deleteUser = asyncHandler(async (req, res) => {
+    res.send("delete user")
+   });
+
+   const updateUser = asyncHandler(async (req, res) => {
+    res.send("update user")
+   });
+
+   export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUsersProfile,
+    getUsers,
+    deleteUser,
+    getUsersById,
+    updateUser,
+   }
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express, { response } from 'express';
 import dotenv from "dotenv";
 import conectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorMiddlewares.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 conectDB();
@@ -11,11 +13,19 @@ const port = process.env.PORT || 5000
 
 const app = express();
 
+//Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//cookie parser middleware
+app.use(cookieParser());
+
 app.get("/", (req, res) =>{
     res.send('API running')
 
 })
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound);
 app.use(errorHandler);
