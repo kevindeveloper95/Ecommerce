188dd3d8f4de52325b136e835566993c4cdc2a1b
kevin <kemen95@hotmail.com>
2024-04-02 17:43:08 -0600

    I do the screen to create new products for the admin and add the routes in the backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,12 @@
 import express  from "express";
 /* import products from '../data/products.js'; */
-import {getProducts,getProductById,} from '../controllers/productController.js';
-
+import {getProducts,getProductById, createProduct,} from '../controllers/productController.js';
+import {protect, admin} from '../middlewares/authMiddleware.js'
 const router = express.Router()
 
 
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
 
 export default router
