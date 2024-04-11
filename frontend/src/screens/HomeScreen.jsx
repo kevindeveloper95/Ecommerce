64a1099b9b6f078d3b1d0366412c1143d319bd41
kevin <kemen95@hotmail.com>
2024-04-10 18:05:23 -0600

    created a paginate components for all the products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import { useGetProductsQuery } from '../slices/productSlice';
+import { Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+import { Link } from 'react-router-dom';
 import Product from '../components/product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Paginate from '../components/Paginate';
+
 
 
 
@@ -13,7 +15,13 @@ import Message from '../components/Message';
 
 const HomeScreen = () => {
 
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber, keyword } = useParams();
+
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
+
   return (
     <>
      {isLoading ? (
@@ -26,14 +34,18 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
            
           </Row>
-        
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            keyword={keyword ? keyword : ''}
+          />
         </>
       )}
     </>
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,70 +1,73 @@
-import { apiSlice } from "./apiSlice";
-import { PRODUCTS_URL } from "../constant";
-
+import { PRODUCTS_URL } from '../constant';
+import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
+  endpoints: (builder) => ({
     getProducts: builder.query({
-        query: () => ({
-            url: PRODUCTS_URL,
-        }),
-        keepUnusedDataFor: 5,
+      query: ({ keyword, pageNumber }) => ({
+        url: PRODUCTS_URL,
+        params: { keyword, pageNumber },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ['Products'],
     }),
     getProductDetails: builder.query({
-        query: (productId) => ({
-            url: `${PRODUCTS_URL}/${productId}`,
-        }),
-        keepUnusedDataFor: 5,
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
     }),
     createProduct: builder.mutation({
-        query: () => ({
-          url: `${PRODUCTS_URL}`,
-          method: 'POST',
-        }),
-        invalidatesTags: ['Product'],
+      query: () => ({
+        url: `${PRODUCTS_URL}`,
+        method: 'POST',
       }),
-      updateProduct: builder.mutation({
-        query: (data) => ({
-          url: `${PRODUCTS_URL}/${data.productId}`,
-          method: 'PUT',
-          body: data,
-        }),
-        invalidatesTags: ['Products'],
+      invalidatesTags: ['Product'],
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: 'PUT',
+        body: data,
       }),
-      uploadProductImage: builder.mutation({
-        query: (data) => ({
-          url: `/api/upload`,
-          method: 'POST',
-          body: data,
-        }),
-      }),
-      deleteProduct: builder.mutation({
-        query: (productId) => ({
-          url: `${PRODUCTS_URL}/${productId}`,
-          method: 'DELETE',
-        }),
-        providesTags: ['Product'],
-      }),
-      createReview: builder.mutation({
-        query: (data) => ({
-          url: `${PRODUCTS_URL}/${data.productId}/reviews`,
-          method: 'POST',
-          body: data,
-        }),
-        invalidatesTags: ['Product'],
-      }),
-      getTopProducts: builder.query({
-        query: () => `${PRODUCTS_URL}/top`,
-        keepUnusedDataFor: 5,
+      invalidatesTags: ['Products'],
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `/api/upload`,
+        method: 'POST',
+        body: data,
       }),
     }),
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: 'DELETE',
+      }),
+      providesTags: ['Product'],
+    }),
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    getTopProducts: builder.query({
+      query: () => `${PRODUCTS_URL}/top`,
+      keepUnusedDataFor: 5,
+    }),
+  }),
 });
 
-export const { useGetProductsQuery,
-    useGetProductDetailsQuery,
-    useCreateProductMutation,
-    useUpdateProductMutation,
-    useUploadProductImageMutation,
-    useDeleteProductMutation,
-    useCreateReviewMutation,
-    useGetTopProductsQuery,} = productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+  useDeleteProductMutation,
+  useCreateReviewMutation,
+  useGetTopProductsQuery,
+} = productsApiSlice;
